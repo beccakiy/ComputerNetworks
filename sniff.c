8e09853572856700d840a56d52166f7cd42f9d5d
Lauren Eagan <lauren0kiy@Laurens-MacBook-Pro.local>
2020-11-01 16:16:01 -0600

    Updates to sniffer4

--- sniff.c
+++ sniff.c
@@ -110,7 +110,7 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
       printf("    Protocol other\n");
       return;
    }
-   printf("print if you made it here");
+  
    //tcp offset
    tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
    size_tcp = TH_OFF(tcp)*4;
@@ -122,20 +122,21 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *pa
    
    size_payload = ntohs(ip->ip_len) - (size_ip + size_tcp);
    
-    if(size_payload > 0){
-        printf("  Payload (%d bytes):\n",size_payload);
-            // ascii (if printable) 
-        const u_char *ch = payload;
-        for(int i=0;i<size_payload;i++){
-            if(isprint(*ch))
-                printf("%c",*ch);
-            else
-                printf(".");
-            ch++;
-        }
-    }
+   if(size_payload > 0){
+      printf("  Payload (%d bytes):\n",size_payload);
+
+      const u_char *ch = payload;
+      for(int i=0;i<size_payload;i++){
+         if(isprint(*ch))
+               printf("%c",*ch);
+         else
+               printf(".");
+         ch++;
+      }
+   }
    
-    return;
+   printf("print if you made it here");
+   return;
 }
 
  
