ab0cd717c491b44ffb7e8180a4b885800184838c
Lauren <lauren0kiy@icloud.com>
2020-11-01 15:37:03 -0500

    Updated Sniffer

--- sniff.c
+++ sniff.c
@@ -1,30 +1,216 @@
 #include <pcap.h>
 #include <stdio.h>
-
+#include <arpa/inet.h>
+#include <ctype.h>
+ 
+#define EHTER_ADDR_LEN 6
+ 
+ 
+ 
 /* This function will be invoked by pcap for each captured packet.
-We can process each packet inside the function.
-*/
-void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet){
-   printf("Got a packet\n");
-}
+We can process each packet inside the function. */
+ 
+/*Ethernet     Header*/
+struct ethheader{
+   u_char  ether_dhost[EHTER_ADDR_LEN];
+   u_char  ether_shost[EHTER_ADDR_LEN];
+   u_short ether_type;
+};
+ 
+/*IP Header*/
+ 
+struct ipheader{
+   u_char  ip_vhl;    
+   u_char  ip_tos;    
+   u_short ip_len;    
+   u_short ip_id;     
+   u_short ip_off;    
+   #define IP_RF 0x8000       
+   #define IP_DF 0x4000       
+   #define IP_MF 0x2000       
+   #define IP_OFFMASK 0x1fff  
+   u_char  ip_ttl;    
+   u_char  iph_protocol;      
+   u_short ip_sum;    
+   struct  in_addr ip_src,ip_dst;
+};
+ 
+#define IP_HL(ip)       (((ip)->ip_vhl) & 0x0f)
+#define IP_V(ip)        (((ip)->ip_vhl) >> 4)
+ 
+/* TCP header */
+typedef u_int tcp_seq;
+ 
+struct sniff_tcp {
+   u_short th_sport;   /* source port */
+   u_short th_dport;   /* destination port */
+   tcp_seq th_seq;     /* sequence number */
+   tcp_seq th_ack;     /* acknowledgement number */
+   u_char th_offx2;   
+   #define TH_OFF(th)  (((th)->th_offx2 & 0xf0) >> 4)
+   u_char th_flags;
+   #define TH_FIN 0x01
+   #define TH_SYN 0x02
+   #define TH_RST 0x04
+   #define TH_PUSH 0x08
+   #define TH_ACK 0x10
+   #define TH_URG 0x20
+   #define TH_ECE 0x40
+   #define TH_CWR 0x80
+   #define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+   u_short th_win;    
+   u_short th_sum;    
+   u_short th_urp;    
+};
+void
+print_hex_ascii_line(const u_char *payload, int len, int offset){
+		int i;
+		int gap;
+		const u_char *ch;
 
+		printf("%05d	",offset);
+
+		ch = payload;
+		for(i = 0; i<len; i++){
+			printf("%02x", *ch);
+			ch++;
+			if(i ==7)
+				printf(" ");
+		}
+
+
+		printf("	");
+
+		ch = payload;
+		for(i = 0; i<len ; i++){
+			if(isprint(*ch))
+				printf("%c", *ch);
+			else 
+				printf(".");
+			ch++;
+		}
+		
+		printf("\n");
+return;
+
+}
+void
+print_payload(const u_char *payload, int len)
+{
+		int len_rem = len;
+		int line_width = 16;
+		int line_len;
+		int offset = 0;
+		const u_char *ch = payload;
+
+		if(len<=0)
+			return;
+
+		if(len<=line_width){
+			print_hex_ascii_line(ch, len, offset);
+			return;
+		}
+
+		for ( ;; ){
+			line_len = line_width % len_rem;
+			print_hex_ascii_line(ch, line_len, offset);
+
+			len_rem= len_rem - line_len;
+			ch = ch + line_len;
+			offset = offset + line_width;
+
+			if(len_rem <= line_width){
+				print_hex_ascii_line(ch, len_rem, offset);
+				break;
+			}
+		}
+
+return;
+}
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet){
+  
+   const struct ethheader *ether;
+   const struct ipheader *ip;
+   const struct sniff_tcp *tcp;
+   const char *payload;
+ 
+   int size_payload;
+   int i;
+ 
+   printf("Got a packet\n");
+   ether =(struct ethheader *)packet;
+ 
+   if(ntohs(ether->ether_type) == 0x800)
+   {
+       ip = (struct ipheader *)(packet + sizeof(struct ethheader));
+      
+       printf("    From: %s\n",inet_ntoa(ip->ip_src));
+       printf("    To: %s\n", inet_ntoa(ip->ip_dst));
+ 
+       switch(ip->iph_protocol){
+           case IPPROTO_TCP:
+               printf("    Protocol is TCP\n");
+               return;
+           case IPPROTO_UDP:
+               printf("    Protocol is UDP\n");
+           case IPPROTO_ICMP:
+               printf("    Protocol is ICMP\n");
+           default:
+               printf("    Protocol other\n");
+       }
+   
+ 
+   tcp = (struct sniff_tcp*)(packet + sizeof(struct ethheader) + sizeof(struct ipheader));
+ 
+   printf("    Source Port%d\n",ntohs(tcp->th_sport));
+   printf("   Dst port: %d\n", ntohs(tcp->th_dport));
+ 
+   payload = (u_char *)packet + sizeof(struct ethheader) + sizeof(struct ipheader) +sizeof(struct sniff_tcp);
+   
+   size_payload = ntohs(ip->ip_len) - (sizeof(struct ipheader) + sizeof(struct sniff_tcp));
+   
+   if(size_payload >0){
+       printf("  payload (%d bytes):\n", size_payload);
+       print_payload(payload, size_payload);
+        
+       }
+   }
+ 
+   return;
+ 
+}
+ 
+ 
+ 
 int main(){
    pcap_t *handle;
    char errbuf[PCAP_ERRBUF_SIZE];
    struct bpf_program fp;
-   char filter_exp[] = "ip proto icmp";
+   char filter_exp[] = "proto \\TCP and (host 10.0.2.5 and 10.0.2.4) and dst port range 10-100";
    bpf_u_int32 net;
-   // Step 1: Open live pcap session on NIC with name ethx
-   // you need to change "eth3" to the name
-   // found on their own machines (using ifconfig).
-   handle = pcap_open_live("ethx", BUFSIZ, 1, 1000, errbuf);
+ 
+ 
+   // Step 1: Open live pcap session on NIC with name eth3
+   //         Students needs to change "eth3" to the name
+   //         found on their own machines (using ifconfig).
+ 
+   handle = pcap_open_live("enp0s3", BUFSIZ, 1, 1000, errbuf);
+ 
    // Step 2: Compile filter_exp into BPF psuedo-code
+ 
    pcap_compile(handle, &fp, filter_exp, 0, net);
+ 
    pcap_setfilter(handle, &fp);
+ 
    // Step 3: Capture packets
+ 
    pcap_loop(handle, -1, got_packet, NULL);
-   pcap_close(handle); //Close the handle 
+   pcap_close(handle);   //Close the handle
    return 0;
 }
-// Note: don't forget to add "-lpcap" to the compilation command.
-// For example: gcc -o sniff sniff.c -lpcapkkk
+ 
+ 
+// Note: don’t forget to add "-lpcap" to the compilation command.
+// For example: gcc -o sniff sniff.c -lpcap
+ 
+
