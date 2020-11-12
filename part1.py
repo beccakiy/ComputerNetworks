b54ae02391a7ef2e80aebb1367ac976e0b02bcbd
Lauren Eagan <lauren0kiy@Laurens-MacBook-Pro.local>
2020-11-12 16:26:18 -0600

    t

--- part1.py
+++ part1.py
@@ -9,17 +9,17 @@ class part1_topo(Topo):
     
     def build(self):
         #Creates the custom topo
-        #pass
+  
         #Creates Hosts and Switches 
         switch1 = self.addSwitch('switch1')
         host1 = self.addHost('host1')
         host2 = self.addHost('host2')
         host3 = self.addHost('host3')
         host4 = self.addHost('host4')
-        self.addLink(host1,switchname)
-        self.addLink(host2,switchname)
-        self.addLink(host3,switchname)
-        self.addLink(host4,switchname)
+        self.addLink(host1,switch1)
+        self.addLink(host2,switch1)
+        self.addLink(host3,switch1)
+        self.addLink(host4,switch1)
         
 topos = {'part1' : part1_topo}
 
